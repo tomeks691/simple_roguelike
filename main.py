e21e3495d1495a0e22d0a0497eb861f95a05c7b7
lucek <luci6969@protonmail.ch>
2022-04-25 20:18:08 +0200

    Add draw map movement player and not going beyond screen

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import tcod
-from actions import EscapeAction, MovementAction
+from engine import Engine
+from entity import Entity
+from game_map import GameMap
 from input_handlers import EventHandler
+
 
 def main() -> None:
     # screen size
     screen_width = 80
     screen_height = 50
-    player_x = int(screen_width / 2)
-    player_y = int(screen_height / 2)
+    map_width = 80
+    map_height = 50
     # Using font
     tileset = tcod.tileset.load_tilesheet(
         "chars.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+    game_map = GameMap(map_width, map_height)
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
     # Create window
     with tcod.context.new_terminal(
             screen_width,
@@ -26,22 +34,10 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F")
         while True:
             # actually put the “@” symbol on the screen in its proper place
-            root_console.print(x=player_x, y=player_y, string="@")
+            engine.render(console=root_console, context=context)
             # Update window
-            context.present(root_console)
-            root_console.clear()
-            for event in tcod.event.wait():
-                action = event_handler.dispatch(event)
-
-                if action is None:
-                    continue
-
-                if isinstance(action, MovementAction):
-                    player_x += action.dx
-                    player_y += action.dy
-
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
+            events = tcod.event.wait()
+            engine.handle_events(events)
 
 
 if __name__ == "__main__":
